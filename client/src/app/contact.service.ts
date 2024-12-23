2d31f7320273a0ce42d95c738ad39d1d68ccc274
Ayas <aya.senhaji16@gmail.com>
2024-12-23 21:14:59 +0100

    Update contact.service.ts

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -13,27 +13,23 @@ export interface Contact {
   providedIn: 'root',
 })
 export class ContactService {
-  private apiUrl = 'http://127.0.0.1:5000/contacts'; // Flask API URL
+  private apiUrl = 'http://127.0.0.1:5000/contacts';
 
   constructor(private http: HttpClient) {}
 
-  // Get all contacts
   getContacts(): Observable<Contact[]> {
     return this.http.get<Contact[]>(this.apiUrl);
   }
 
-  // Add a new contact
   addContact(contact: Contact): Observable<Contact> {
-    return this.http.post<Contact>('http://127.0.0.1:5000/contacts', contact); // Use the correct URL
+    return this.http.post<Contact>('http://127.0.0.1:5000/contacts', contact); 
   }
 
-  // Update a contact
   updateContact(contactId: string, updatedData: any): Observable<any> {
     const url = `http://127.0.0.1:5000/contacts/${contactId}`;
     return this.http.put(url, updatedData);
   }
 
-  // Delete a contact
   deleteContact(name: string): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${name}`);
   }
