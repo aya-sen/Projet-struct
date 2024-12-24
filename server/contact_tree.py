f408e52426ae2519993456f153c82f1c45c5bedc
Ayas <aya.senhaji16@gmail.com>
2024-12-24 19:31:12 +0100

    Update contact_tree.py

--- server/contact_tree.py
+++ server/contact_tree.py
@@ -24,32 +24,45 @@ class ContactTree:
             node.left = self._insert_recursive(node.left, contact)
         else:
             node.right = self._insert_recursive(node.right, contact)
-
        
         node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
 
-        
         balance = self._get_balance(node)
-
-        
+        #rotation droite simple ( left left)
         if balance > 1 and contact["name"].lower() < node.left.contact["name"].lower():
             return self._rotate_right(node)
-
-       
+       #rotation gauche simple (Right Right)
         if balance < -1 and contact["name"].lower() > node.right.contact["name"].lower():
             return self._rotate_left(node)
-
-       
+       # rotation gauche-droite (Left-Right)
         if balance > 1 and contact["name"].lower() > node.left.contact["name"].lower():
             node.left = self._rotate_left(node.left)
             return self._rotate_right(node)
-
-        
+        #rotation droite-gauche (Right-Left)
         if balance < -1 and contact["name"].lower() < node.right.contact["name"].lower():
             node.right = self._rotate_right(node.right)
             return self._rotate_left(node)
 
         return node
+
+    def update(self, contact_id, new_data):
+    
+    
+        contact = self.find_by_id(contact_id)
+        if not contact:
+            return False 
+
+       
+        old_name = contact["name"]
+        if "name" in new_data and new_data["name"].lower() != old_name.lower():
+            self.delete(old_name)  
+            updated_contact = {**contact, **new_data}  
+            self.insert(updated_contact)  
+        else:
+           
+            contact.update(new_data)
+        
+        return True
 
     def find_by_id(self, id):
         """Find a contact by its unique ID."""
@@ -77,37 +90,31 @@ class ContactTree:
             node.left = self._delete_recursive(node.left, name)
         elif name.lower() > node.contact["name"].lower():
             node.right = self._delete_recursive(node.right, name)
-        else:
-           
+        else:#trouvé
+            # un seul enfant
             if not node.left:
                 return node.right
             elif not node.right:
                 return node.left
-
+            # deux enfant 
             temp = self._min_value_node(node.right)
             node.contact = temp.contact
             node.right = self._delete_recursive(node.right, temp.contact["name"])
 
-       
         node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
 
-       
         balance = self._get_balance(node)
-
-       
+       #left left 
         if balance > 1 and self._get_balance(node.left) >= 0:
             return self._rotate_right(node)
-
-       
+       #left right
         if balance > 1 and self._get_balance(node.left) < 0:
             node.left = self._rotate_left(node.left)
             return self._rotate_right(node)
-
-       
+       #right right 
         if balance < -1 and self._get_balance(node.right) <= 0:
             return self._rotate_left(node)
-
-        
+        #right left
         if balance < -1 and self._get_balance(node.right) > 0:
             node.right = self._rotate_right(node.right)
             return self._rotate_left(node)
@@ -115,7 +122,6 @@ class ContactTree:
         return node
 
     def _rotate_left(self, z):
-        """Perform a left rotation."""
         y = z.right
         T2 = y.left
 
