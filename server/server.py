58cbe56cbf03ed49f2110fc7b5c8fa7c0296f80f
Ayas <aya.senhaji16@gmail.com>
2024-12-24 19:30:36 +0100

    Update server.py

--- server/server.py
+++ server/server.py
@@ -17,8 +17,6 @@ def get_contacts():
     return jsonify(contacts)
 
 
-
-
 @app.route('/contacts', methods=['POST'])
 def add_contact():
     new_contact = request.get_json()
@@ -26,21 +24,17 @@ def add_contact():
     return jsonify(new_contact), 201
 
 
-
-
 @app.route('/contacts/<contact_id>', methods=['PUT'])
 def update_contact(contact_id):
     try:
-        
-        contact = contact_tree.find_by_id(contact_id) 
-        if not contact:
+        updated_data = request.get_json()
+
+        update_success = contact_tree.update(contact_id, updated_data)
+
+        if not update_success:
             return jsonify({'message': 'Contact not found'}), 404
-        
 
-        data = request.get_json()
-        contact.update(data)  
-
-        return jsonify(contact), 200 
+        return jsonify({'message': 'Contact updated successfully'}), 200
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({'message': 'Failed to update contact'}), 500
